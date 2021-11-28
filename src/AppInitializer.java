9482f070d25c16a37cbca0aa658d5c278d63dfb9
Pathum456 <pathumkaleesha618@gmail.com>
2021-11-28 18:11:46 +0530

    create configuration file

---
+++ src/AppInitializer.java
@@ -0,0 +1,20 @@
+/**
+ * @author _ Pathum_Kaleesha
+ * @created - 11/28/2021 - 5:05 PM
+ * @since - v0.1.0 - thogakade
+ **/
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch (args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/util/FactoryConfiguration.java
@@ -0,0 +1,11 @@
+package util;
+
+/**
+ * @author _ Pathum_Kaleesha
+ * @created - 11/28/2021 - 5:07 PM
+ * @day - 1
+ * @since - v0.1.0 - thogakade
+ **/
+
+public class FactoryConfiguration {
+}
